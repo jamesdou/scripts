eaacc297603c5ccfb10fa3568cfe63c0104afec8
jamesdou <jamesdou@umich.edu>
2025-07-12 08:29:13 -0400

    added backend start docker compose (dc) and front start npm (npm start)

--- amcp-tool-test.bat
+++ amcp-tool-test.bat
@@ -1,4 +1,2 @@
 @echo off
-  curl -H "X-API-KEY: supersecret" -H "Content-Type: application/json" -X POST
-  http://localhost:8080/amcp/components/auto_tools/execute -d "{\"input\": {\"text\": \"Hello AMCP!\"}}"
-  pause
+  curl -H "X-API-KEY: supersecret" -H "Content-Type: application/json" -X POST  http://localhost:8080/amcp/components/auto_tools/execute -d "{\"input\": {\"text\": \"Hello AMCP!\"}}"  pause
